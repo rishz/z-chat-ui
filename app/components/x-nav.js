fb2f7651f0656aa645bbb6d9b97f787c5c083857
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-11-18 16:08:27 +0530

    Linked nav-link to div

--- app/components/x-nav.js
+++ app/components/x-nav.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['navbar-fixed', 'x-nav'],
+
+  _setupChildComponent(childComponent) {
+    if (childComponent.classNames.indexOf('side-nav') >= 0) {
+      this.set('_sideNavId', childComponent.elementId);
+    }
+  }
 });
