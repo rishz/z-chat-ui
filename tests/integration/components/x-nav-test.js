b59d8c03028cfa13ea2c3327c79077622baffdc7
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-11-18 16:26:37 +0530

    jQuery plugin initialization

--- tests/integration/components/x-nav-test.js
+++ tests/integration/components/x-nav-test.js
@@ -6,13 +6,6 @@ moduleForComponent('x-nav', 'Integration | Component | x nav', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{x-nav}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
   // Template block usage:
   this.render(hbs`
     {{#x-nav}}
@@ -20,5 +13,5 @@ test('it renders', function(assert) {
     {{/x-nav}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim().replace(/[\s\n]+/g, ''), 'menutemplateblocktext');
 });
